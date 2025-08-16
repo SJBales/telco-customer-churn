11fc876e31b4450008bcd63759e2c23d4c920a3f
Samuel Joseph <sambales@Samuels-MacBook-Pro.local>
2025-08-16 15:08:46 -0500

    Added scipy

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -7,4 +7,11 @@ logging.basicConfig(level=logging.INFO, format="%(name)s - %(message)s")
 
 # Importing the data
 raw_data = telco_data_loader()
+
+# cleaning the data
 data_for_fitting = processTelcoData(raw_data)
+
+# Training the model
+regressor = LogisticRegression()
+
+fitted_model = regressor.fit(data_for_fitting["predictors"], data_for_fitting["target"])
