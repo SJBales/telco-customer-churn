eb2ce689a8644e9aae5dee89949750ffe927c6f7
Samuel Joseph <sambales@Samuels-MacBook-Pro.local>
2025-08-18 20:59:34 -0500

    Added cross validation for logistic regression

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,9 +1,15 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 import logging
 from data_loader import telco_data_loader
 from data_processor import processTelcoData
 
+# Setting up a logger
 logging.basicConfig(level=logging.INFO, format="%(name)s - %(message)s")
+
+logger = logging.getLogger(__name__)
 
 # Importing the data
 raw_data = telco_data_loader()
@@ -11,7 +17,29 @@ raw_data = telco_data_loader()
 # cleaning the data
 data_for_fitting = processTelcoData(raw_data)
 
-# Training the model
-regressor = LogisticRegression()
+# Creating a pipeline
+pipeline = Pipeline(
+    [("scaler", StandardScaler()), ("logistic", LogisticRegression(max_iter=1000))]
+)
 
-fitted_model = regressor.fit(data_for_fitting["predictors"], data_for_fitting["target"])
+# creating a cv engine
+cv = StratifiedKFold(n_splits=5, shuffle=True)
+
+# Creating a dict of scoring
+scoring = {"accuracy": "accuracy"}
+
+# running the pipeline
+cv_results = cross_validate(
+    pipeline,
+    X=data_for_fitting["predictors"],
+    y=data_for_fitting["target"],
+    cv=cv,
+    scoring=scoring,
+    return_train_score=True,
+    n_jobs=1,
+)
+
+# logging.INFO("Cross validation ran successfully")
+
+# logging the results
+logger.info(cv_results["test_accuracy"])
